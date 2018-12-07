2c44173feab68a0da80c26332e6e91d5920be573
sitepoint <758563+mmatt@users.noreply.github.com>
2018-12-07 15:11:43 +0100

    Change code to run multiple tasks in parallel

--- c3scooter.ino
+++ c3scooter.ino
@@ -5,10 +5,31 @@
 const int kDisplayCLKpin  = 3;
 const int kdisplayDIOpin  = 2;
 const int kledDINpin      = 6;
+const int kbuttonDINpin   = 5;
 const int kledNum         = 89;
 
+const uint8_t kwordBye_[] = { 0b01111111, 0b01101110, 0b01111001, 0x00 };
+const uint8_t kword_bye[] = { 0x00, 0b01111111, 0b01101110, 0b01111001 };
+const uint8_t kwordHey_[] = { 0b01110110, 0b01111001, 0b01101110, 0x00 };
+const uint8_t kword_hey[] = { 0x00, 0b01110110, 0b01111001, 0b01101110 };
+const uint8_t kwordDots[] = { 0x00, 0b10000000, 0x00, 0x00 };
+const uint8_t kwordBlnk[] = { 0x00, 0x00, 0x00, 0x00 };
+
 // Variables
-unsigned int i;
+unsigned int  i;
+unsigned long current_ms      = 0;
+
+unsigned long button_ms       = 0;
+unsigned int  button_state    = 0;
+unsigned int  button_block    = 2000;
+
+unsigned int  park_mode       = 0;
+unsigned long park_dot_ms     = 0;
+unsigned int  park_dot_state  = 0;
+
+unsigned int  drive_glitch    = 0;
+unsigned long drive_dot_ms    = 0;
+unsigned int  drive_dot_state = 0;
 
 #define BRIGHTNESS  64
 CRGB leds[kledNum];
@@ -29,110 +50,104 @@ TM1637Display display(kDisplayCLKpin, kdisplayDIOpin);
 // Bits are set: XGFEDCBA
 // setBrightness: 0 to 7
 
-#define SIG_B   0b01111111
-#define SIG_Y   0b01101110
-#define SIG_E   0b01111001
-#define BLANK   0x00
-#define DOTS    0b10000000
-
-#define LIN_U   0b00000001
-#define LIN_M   0b01000000
-#define LIN_D   0b00001000
-
-const uint8_t strBYE_[] = { SIG_B, SIG_Y, SIG_E, BLANK };
-const uint8_t str_BYE[] = { BLANK, SIG_B, SIG_Y, SIG_E };
-const uint8_t strLINU[] = { LIN_U, LIN_U, LIN_U, LIN_U };
-const uint8_t strLINM[] = { LIN_M, LIN_M, LIN_M, LIN_M };
-const uint8_t strLIND[] = { LIN_D, LIN_D, LIN_D, LIN_D };
-const uint8_t strDOTS[] = { BLANK, DOTS,  BLANK, BLANK };
-const uint8_t strBLCK[] = { BLANK, BLANK, BLANK, BLANK };
-
-void brightDown(int data) {
-  for (i = 7; i > 0; i--) {
-    display.setBrightness(i);
-    display.setSegments(data);
-    delay(250);
-  }
-  display.setBrightness(7, false);
-  display.setSegments(data);
-}
-
-void brightUp() {
-  for (i = 0; i < 7; i++) {
-    display.setBrightness(i);
-    delay(100);
-  }
-}
-
 int extractDigit(size_t digit, int n) {
   int mask = 1;
-  Serial.print("Extracting: ");
-  Serial.print(n);
-  Serial.println();
+//  Serial.print("Extracting: ");
+//  Serial.print(n);
+//  Serial.println();
   while ( digit -- > 0 )
     mask *= 10;
-  Serial.print("Masking with mask: ");
-  Serial.print(mask);
-  Serial.println();
+//  Serial.print("Masking with mask: ");
+//  Serial.print(mask);
+//  Serial.println();
   if (n < mask / 10) { // insufficient digits
-    Serial.print("Return 0");
-    Serial.println();
+//    Serial.print("Return 0");
+//    Serial.println();
     return display.encodeDigit(0);
   }
   while ( n >= mask )
     n /= mask;
-  Serial.print("Extracted: ");
-  Serial.print(n % 10);
-  Serial.println();
+//  Serial.print("Extracted: ");
+//  Serial.print(n % 10);
+//  Serial.println();
   return display.encodeDigit(n % 10);
 }
 
-void printTimer() {
-  unsigned int up, hrs, mins;
-  uint8_t timer[] = { 0xff, 0xff, 0xff, 0xff };
-
-  // Read milliseconds and add half a second  to round minutes
-  up = (millis() / 1000) + 30;
-  mins = up / 60;
-  hrs  = mins / 60;
-  mins = mins % 60;
-
-  Serial.print("Uptime: ");
-  Serial.print(hrs);
-  Serial.print(":");
-  Serial.print(mins);
-  Serial.println();
-  
-  if (hrs >= 0 && hrs < 10) {
-    timer[0] = 0x00;
-    timer[1] = extractDigit(1, hrs);
-  } else {
-    timer[0] = extractDigit(1, hrs);
-    timer[1] = extractDigit(2, hrs);
-
-  }
-  if (mins >= 0 && mins < 10) {
-    timer[2] = display.encodeDigit(0);
-    timer[3] = extractDigit(1, mins);
-  } else {
-    timer[2] = extractDigit(1, mins);
-    timer[3] = extractDigit(2, mins);
-  }
-
-  // Blinking dots
-  for (i = 0; i < 2; i++) {
-    timer[1] = timer[1] + 128;
-    display.setSegments(timer);
-    delay(1000);
-    timer[1] = timer[1] - 128;
-    display.setSegments(timer);
-    delay(1000);
-  }
-  display.clear();
-}
 
 void glitchC3() {
 
+}
+
+void displayShiftPark() {
+  
+  display.setSegments(kwordBye_);
+  delay(450);
+  display.setSegments(kword_bye);
+  delay(200);
+}
+
+void displayShiftDrive() {
+  display.setSegments(kwordHey_);
+  delay(450);
+  display.setSegments(kword_hey);
+  delay(200);
+}
+
+void displayPark() {
+  if (park_dot_state == 0 && (current_ms - park_dot_ms) > 2000) {
+    // turn it on
+    display.setSegments(kwordDots);
+//    Serial.print("Turn on after ");
+//    Serial.print((current_ms - park_dot_ms));
+//    Serial.println();
+    park_dot_state = 1;
+    park_dot_ms = current_ms;
+  } else if (park_dot_state == 1 && (current_ms - park_dot_ms) > 100) {
+    // turn it off
+    display.setSegments(kwordBlnk);
+//    Serial.print("Turn off after ");
+//    Serial.print((current_ms - park_dot_ms));
+//    Serial.println();
+    park_dot_state = 0;
+    park_dot_ms = current_ms;
+  }
+}
+
+void displayDrive() {
+  if ((current_ms - drive_dot_ms) > 1000) {
+    
+    unsigned int up, hrs, mins;
+    uint8_t timer[] = { 0xff, 0xff, 0xff, 0xff };
+  
+    // Add half a second to round minutes
+    mins = ((current_ms / 1000) + 30) / 60;
+    hrs  = mins / 60;
+    mins = mins % 60;
+    
+    if (hrs >= 0 && hrs < 10) {
+      timer[0] = 0x00;
+      timer[1] = extractDigit(1, hrs);
+    } else {
+      timer[0] = extractDigit(1, hrs);
+      timer[1] = extractDigit(2, hrs);
+    }
+    if (mins >= 0 && mins < 10) {
+      timer[2] = display.encodeDigit(0);
+      timer[3] = extractDigit(1, mins);
+    } else {
+      timer[2] = extractDigit(1, mins);
+      timer[3] = extractDigit(2, mins);
+    }
+
+    if (drive_dot_state == 0) {
+      timer[1] = timer[1] + 128;
+      drive_dot_state = 1;
+    } else {
+      drive_dot_state = 0;
+    }
+    drive_dot_ms = current_ms;
+    display.setSegments(timer);
+  }
 }
 
 ////////////////
@@ -149,43 +164,56 @@ void setup() {
   Serial.print("Start");
   Serial.println();
 
-  delay( 3000 ); // power-up safety delay
+  delay(3000);
+
+  // Initialize LED strip  
   FastLED.addLeds<WS2812B, kledDINpin, GRB>(leds, kledNum).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
+
+  // Initialize 7 segment display
+  display.setBrightness(7, true);
+
+  pinMode(kbuttonDINpin, INPUT);
 }
 
 void loop() {
+  current_ms = millis();
   
-  display.setBrightness(7);
-  for (i = 0; i < 5; i++) {
-    display.setSegments(strLINU);
-    delay(250);
-    display.setSegments(strLINM);
-    delay(250);
-    display.setSegments(strLIND);
-    delay(250);
-  }
-  display.setSegments(strBYE_);
-  delay(450);
-  display.setSegments(str_BYE);
-  delay(200);
-  brightDown(str_BYE);
-  delay(100);
-  display.setBrightness(7, true);
-  for (i = 0; i < 2; i++) {
-    display.setSegments(strDOTS);
-    delay(100);
-    display.setSegments(strBLCK);
-    delay(2000);
+  button_state = digitalRead(kbuttonDINpin);
+  if (button_state == HIGH && park_mode == 0 && (current_ms - button_ms) > button_block) {
+    // enter parking mode
+    displayShiftPark();
+    park_mode = 1;
+    button_ms = current_ms;
+  } else if (button_state == HIGH && park_mode == 1 && (current_ms - button_ms) > button_block) {
+    displayShiftDrive();
+    park_mode = 0;
+    button_ms = current_ms;
   }
 
-  printTimer();
-  delay(1000);
+  if (park_mode == 1) {
+    displayPark();
+    for(int i = 0; i < kledNum; i++) {
+      leds[i] = CRGB::Blue;    // set our current dot to red
+      FastLED.show();
+      leds[i] = CRGB::Black;  // set our current dot to black before we continue
+    }
+  } else {
+    displayDrive();
+    for(int i = 0; i < kledNum; i++) {
+      leds[i] = CRGB::Red;    // set our current dot to red
+      FastLED.show();
+      leds[i] = CRGB::Black;  // set our current dot to black before we continue
+    }
+  }
+
+  
+  //delay(1000);
   
 
-  for(int i = 0; i < kledNum; i++) {
-    leds[i] = CRGB::Blue;    // set our current dot to red
-    FastLED.show();
-    leds[i] = CRGB::Black;  // set our current dot to black before we continue
-  }
+  
+  
+  Serial.print("This loop took ");
+  Serial.print((millis() - current_ms));
+  Serial.println();  
 }

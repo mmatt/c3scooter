d7df46fd960f9d38ec4294f5be50e1b773f2e569
sitepoint <758563+mmatt@users.noreply.github.com>
2018-12-17 21:18:27 +0100

    Add flashing lights to park mode

--- c3scooter.ino
+++ c3scooter.ino
@@ -1,28 +1,28 @@
 #include <TM1637Display.h>
 #include <FastLED.h>
-#include <Wire.h>
-#include "MPU6050.h"
+#include <MPU6050.h>
 
 // Constants
-const int     kDisplayCLKpin  = 8;
-const int     kdisplayDIOpin  = 7;
-const int     kgyroMPUaddr    = 0x68;
-const int     kledDINpin      = 9;
-const int     kbuttonDINpin   = 10;
-const int     kledNum         = 80;
+const int     kVersion        = 1;
+const int     kDisplayClkPin  = 8;
+const int     kDisplayDioPin  = 7;
+const int     kGyroMpuAddr    = 0x68;
+const int     kLedDinPin      = 9;
+const int     kButtonDinPin   = 10;
+const int     kLedNum         = 80;
+const int     kLedBrightness  = 64;
 
 const uint8_t kwordBye_[]     = { 0b01111111, 0b01101110, 0b01111001, 0x00 };
 const uint8_t kword_bye[]     = { 0x00, 0b01111111, 0b01101110, 0b01111001 };
 const uint8_t kwordHey_[]     = { 0b01110110, 0b01111001, 0b01101110, 0x00 };
 const uint8_t kword_hey[]     = { 0x00, 0b01110110, 0b01111001, 0b01101110 };
-const uint8_t kwordCALI[]     = { 0b00111001, 0b01110111, 0b00111000, 0b00110000 };
-const uint8_t kwordSOS_[]     = { 0b01101101, 0b00111111, 0b01101101, 0x00 };
-const uint8_t kword_SOS[]     = { 0x00, 0b01101101, 0b00111111, 0b01101101 };
+const uint8_t kwordSos_[]     = { 0b01101101, 0b00111111, 0b01101101, 0x00 };
+const uint8_t kword_sos[]     = { 0x00, 0b01101101, 0b00111111, 0b01101101 };
 const uint8_t kwordDots[]     = { 0x00, 0b10000000, 0x00, 0x00 };
 const uint8_t kwordBlnk[]     = { 0x00, 0x00, 0x00, 0x00 };
 
 // Variables
-unsigned int  i;
+unsigned int  i, j;
 unsigned long current_ms      = 0;
 
 unsigned long button_ms       = 0;
@@ -32,25 +32,32 @@ unsigned int  button_block    = 2000;
 unsigned int  park_mode       = 1;
 unsigned long park_dot_ms     = 0;
 unsigned int  park_dot_state  = 0;
+unsigned long park_flash_ms   = 0;
+unsigned int  park_flash_state= 0;
 unsigned long park_gyro_ms    = 0;
 unsigned int  park_gyro_alert = 0;
+unsigned long park_sos_ms     = 0;
+unsigned int  park_sos_state  = 0;
 
 unsigned int  drive_glitch    = 0;
 unsigned long drive_dot_ms    = 0;
 unsigned int  drive_dot_state = 0;
 
 int16_t       accel_x, accel_y, accel_z;
+int16_t       accel_x_prev, accel_y_prev, accel_z_prev;
+int16_t       accel_total, accel_calc;
 int16_t       gyro_x, gyro_y, gyro_z;
-int16_t temperature;
+int16_t       gyro_x_prev, gyro_y_prev, gyro_z_prev;
+int16_t       gyro_total, gyro_calc;
+int16_t       temperature;
 
-#define BRIGHTNESS  64
-CRGB leds[kledNum];
+CRGB leds[kLedNum];
 
 ////////////////////////////
 // 7-SEGMENT DISPLAY CODE //
 ////////////////////////////
 
-TM1637Display display(kDisplayCLKpin, kdisplayDIOpin);
+TM1637Display display(kDisplayClkPin, kDisplayDioPin);
 
 //
 //  |-A-|
@@ -64,24 +71,13 @@ TM1637Display display(kDisplayCLKpin, kdisplayDIOpin);
 
 int extractDigit(size_t digit, int n) {
   int mask = 1;
-//  Serial.print("Extracting: ");
-//  Serial.print(n);
-//  Serial.println();
   while ( digit -- > 0 )
     mask *= 10;
-//  Serial.print("Masking with mask: ");
-//  Serial.print(mask);
-//  Serial.println();
   if (n < mask / 10) { // insufficient digits
-//    Serial.print("Return 0");
-//    Serial.println();
     return display.encodeDigit(0);
   }
   while ( n >= mask )
     n /= mask;
-//  Serial.print("Extracted: ");
-//  Serial.print(n % 10);
-//  Serial.println();
   return display.encodeDigit(n % 10);
 }
 
@@ -105,7 +101,9 @@ void displayShiftDrive() {
 }
 
 void displayPark() {
-  if (park_dot_state == 0 && (current_ms - park_dot_ms) > 2000) {
+  if (park_sos_ms > 0) {
+    displaySos();
+  } else if (park_dot_state == 0 && (current_ms - park_dot_ms) > 2000) {
     // turn it on
     display.setSegments(kwordDots);
 //    Serial.print("Turn on after ");
@@ -158,6 +156,22 @@ void displayDrive() {
     }
     drive_dot_ms = current_ms;
     display.setSegments(timer);
+
+    // Reset SOS in drive mode 
+    park_flash_ms = 0;
+  }
+}
+
+void displaySos() {
+  if ((current_ms - park_sos_ms) > 250) {
+    if (park_sos_state == 0) {
+      display.setSegments(kwordSos_);
+      park_sos_state = 1;
+    } else {
+      display.setSegments(kword_sos);
+      park_sos_state = 0;
+    }
+    park_sos_ms = current_ms;
   }
 }
 
@@ -168,49 +182,115 @@ void displayDrive() {
 // SCL to pin A5
 // SDA to pin A4
 
-char tmp_str[7];
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
-  sprintf(tmp_str, "%6d", i);
-  return tmp_str;
-}
-
 void readGyro() {
-  
-      Serial.print("Time diff is ");
+
+  /*
+  Serial.print("Time diff is ");
   Serial.print(current_ms - park_gyro_ms);
   Serial.println();
+  */
   
-  if ((current_ms - park_gyro_ms) > 1000) {
+  if ((current_ms - park_gyro_ms) > 50) {
 
   
-    Wire.beginTransmission(kgyroMPUaddr);
-    Wire.write(0x3B); // start with 0x3B (ACCEL_XOUT_H)
+    Wire.beginTransmission(kGyroMpuAddr);
+    //Wire.write(0x3B); // start with 0x3B (ACCEL_XOUT_H)
+    Wire.write(0x43); 
     Wire.endTransmission(false);
-    Wire.requestFrom(kgyroMPUaddr, 7*2, true); // request 14 registers
+    Wire.requestFrom(kGyroMpuAddr, 7*2, true); // request 14 registers
   
     // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-    accel_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
     /*
+    accel_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+    
     accel_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+    
     accel_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
     temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+    */
     gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
     gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
     gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
-    */
-
     
-    Serial.print("aX = "); Serial.print(convert_int16_to_str(accel_x));
+    //accel_total = 0;
+    //accel_total = abs(accel_x_prev - accel_x) + abs(accel_y_prev - accel_y) + abs(accel_z_prev - accel_z);
+    gyro_total = abs(gyro_x_prev - gyro_x) + abs(gyro_y_prev - gyro_y) + abs(gyro_z_prev - gyro_z);
     /*
+    if (accel_x_prev > accel_x) {
+      accel_calc = accel_x_prev - accel_x;
+    } else {
+      
+    }*/
+    
+
+    //Serial.print("TEST "); Serial.print(accel_total);
+    //Serial.print("Gyro Total "); Serial.print(gyro_total);
+    /*
+    Serial.print("aX = "); Serial.print(convert_int16_to_str(accel_x));
+    
     Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accel_y));
+    
     Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accel_z));
     // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
     Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
     Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
     Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
     Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
-    */
     Serial.println();
+    accel_x_prev = accel_x;
+    accel_y_prev = accel_y;
+    accel_z_prev = accel_z;
+    */
+
+    // use gyros to calculate height of alert bar
+    if ((gyro_total > 250 && park_gyro_alert == 0) || (gyro_total > 150 && park_gyro_alert > 0)) {
+      if (park_gyro_alert < kLedNum) {
+        park_gyro_alert+=2;
+        if (park_flash_state == 1) {
+          fill_solid( &(leds[i]), park_gyro_alert, CRGB::Red );
+        }
+      }
+    } else if (gyro_total < 100 && park_gyro_alert > 0) {
+      park_gyro_alert--;
+      if (park_flash_state == 1) {
+        fill_solid( &(leds[i]), park_gyro_alert, CRGB::Red );
+        leds[park_gyro_alert] = CRGB::Black;
+      }
+    } 
+
+    // Alert flashing
+    if ((current_ms - park_flash_ms) > 200) {
+      if (park_flash_state == 0) {
+        fill_solid( &(leds[i]), park_gyro_alert, CRGB::White );
+        park_flash_state = 1;
+      } else {
+        // Only flash over certain threshold
+        if (park_gyro_alert > 40) {
+          fill_solid( &(leds[i]), kLedNum, CRGB::Black );
+          park_flash_state = 0;
+        }
+      }
+      park_flash_ms = current_ms;
+    } 
+    FastLED.show();
+
+    if (park_gyro_alert == kLedNum) {
+      displaySos();
+      for(j = 0; j < 30; j++) {
+        fill_solid( &(leds[i]), kLedNum, CRGB::White );
+        FastLED.show();
+        delay(50);
+        fill_solid( &(leds[i]), kLedNum, CRGB::Black );
+        FastLED.show();
+        delay(50);
+      }
+    }
+
+    gyro_x_prev = gyro_x;
+    gyro_y_prev = gyro_y;
+    gyro_z_prev = gyro_z;
+    //Serial.print("Gyro alert "); Serial.print(park_gyro_alert);
+    //Serial.println();
     park_gyro_ms = current_ms;
   }
 }
@@ -229,21 +309,23 @@ void setup() {
   Serial.print("Start");
   Serial.println();
 
-  delay(3000);
-
-  // Initialize LED strip  
-  FastLED.addLeds<WS2812B, kledDINpin, GRB>(leds, kledNum).setCorrection( TypicalLEDStrip );
-  FastLED.setBrightness(  BRIGHTNESS );
-
   // Initialize 7 segment display
   display.setBrightness(2, true);
 
+  // Show version number
+  display.showNumberDecEx(kVersion, 0x80, true, 2, 1);
+  delay(3000);
+
+  // Initialize LED strip  
+  FastLED.addLeds<WS2812B, kLedDinPin, GRB>(leds, kLedNum).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(kLedBrightness);
+
   // Initizialize button
-  pinMode(kbuttonDINpin, INPUT);
+  pinMode(kButtonDinPin, INPUT);
 
   // Initialize gyroscope communication
   Wire.begin();
-  Wire.beginTransmission(kgyroMPUaddr);
+  Wire.beginTransmission(kGyroMpuAddr);
   Wire.write(0x6B); // PWR_MGMT_1 register
   Wire.write(0); // wake up the MPU-6050
   Wire.endTransmission(true);
@@ -253,7 +335,7 @@ void loop() {
   current_ms = millis();
 
   // Check button state
-  button_state = digitalRead(kbuttonDINpin);
+  button_state = digitalRead(kButtonDinPin);
   if (button_state == HIGH && park_mode == 0 && (current_ms - button_ms) > button_block) {
     // enter parking mode
     Serial.print("Enter parking mode");
@@ -276,15 +358,17 @@ void loop() {
     // We are parked
     displayPark();
     readGyro();
-    for(int i = 0; i < kledNum; i++) {
+    /*
+    for(int i = 0; i < kLedNum; i++) {
       leds[i] = CRGB::Blue;    // set our current dot to red
       FastLED.show();
       leds[i] = CRGB::Black;  // set our current dot to black before we continue
     }
+    */
   } else {
     // We're in drive
     displayDrive();
-    for(int i = 0; i < kledNum; i++) {
+    for(i = 0; i < kLedNum; i++) {
       leds[i] = CRGB::Red;    // set our current dot to red
       FastLED.show();
       leds[i] = CRGB::Black;  // set our current dot to black before we continue
@@ -294,8 +378,10 @@ void loop() {
   
   //delay(100);
   
-
+  /*
   Serial.print("This loop took ");
   Serial.print((millis() - current_ms));
   Serial.println();
+  */
+  
 }
